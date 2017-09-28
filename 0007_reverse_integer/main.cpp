a59110f2c454bc351dc28106730e8a62b070b0d5
HuangTingyu <TingyuHuang@users.noreply.github.com>
2017-09-29 00:55:07 +0800

    <leetcode> #7 - Reverse Integer (C++)
        1032 / 1032 test cases passed.
        Status: Accepted
        Runtime: 12 ms
    
    Signed-off-by: HuangTingyu <TingyuHuang@users.noreply.github.com>

---
+++ 0007_reverse_integer/main.cpp
@@ -0,0 +1,43 @@
+class Solution {
+	public:
+		/**
+		 *  Checking overflow
+		 *
+		 *  It's not overflowing if (INT_MAX >= x_reversed * 10 + val)
+		 *  is true. which means ((INT_MAX - val) / 10 < x_reversed) is
+		 *  true when it overflowing.
+		 */
+		bool is_overflow(const int x_reversed, const int val) {
+			return ((std::numeric_limits<int>::max() - val) / 10 < x_reversed);
+		}
+
+		int reverse(int x) {
+			bool negative = (x < 0);
+			int x_reversed = 0;
+
+			if (0 == x) {
+				return 0;
+			}
+
+			if (negative) {
+				x = -x;
+			}
+
+			while (x != 0) {
+				int val = x % 10;
+				x /= 10;
+
+				if (is_overflow(x_reversed, val)) {
+					return 0;
+				} else {
+					x_reversed = x_reversed * 10 + val;
+				}
+			}
+
+			if (negative) {
+				return -x_reversed;
+			} else {
+				return x_reversed;
+			}
+		}
+};
