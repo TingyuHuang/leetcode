26b371fe1f821faff313081a5c5b18511aa03cda
HuangTingyu <TingyuHuang@users.noreply.github.com>
2017-09-29 01:17:02 +0800

    <leetcode> #2 - use auto
    Signed-off-by: HuangTingyu <TingyuHuang@users.noreply.github.com>

--- 0002_add_two_numbers/main.cpp
+++ 0002_add_two_numbers/main.cpp
@@ -9,14 +9,14 @@
 class Solution {
 	public:
 		ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-			ListNode head = ListNode(0);
-			ListNode *last = &head;
-			int carry = 0;
+			auto head = ListNode(0);
+			auto *last = &head;
+			auto carry = 0;
 
 			while (nullptr != l1 || nullptr != l2 || 0 != carry) {
-				int l1val = l1 ? l1->val : 0;
-				int l2val = l2 ? l2->val : 0;
-				int sum = l1val + l2val + carry;
+				auto l1val = l1 ? l1->val : 0;
+				auto l2val = l2 ? l2->val : 0;
+				auto sum = l1val + l2val + carry;
 
 				last->next = new ListNode(sum % 10);
 				carry = sum / 10;
